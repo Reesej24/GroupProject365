962557ec124acd7d5df9543616c8c4e299ab3ebe
spenc <spenc@archSpenc>
2025-05-05 13:28:11 -0700

    added potion logic to the repo

--- src/api/server.py
+++ src/api/server.py
@@ -1,39 +1,27 @@
 from fastapi import FastAPI
-from src.api import carts, catalog, bottler, barrels, admin, info, inventory
+from src.api import hi, auth
 from starlette.middleware.cors import CORSMiddleware
 
 description = """
-Central Coast Cauldrons is the premier ecommerce site for all your alchemical desires.
+Course Filer is our Group project which helps students find courses that fit their schedule.
 """
 tags_metadata = [
-    {"name": "cart", "description": "Place potion orders."},
-    {"name": "catalog", "description": "View the available potions."},
-    {"name": "bottler", "description": "Bottle potions from the raw magical elixir."},
-    {
-        "name": "barrels",
-        "description": "Buy barrels of raw magical elixir for making potions.",
-    },
-    {"name": "admin", "description": "Where you reset the game state."},
-    {"name": "info", "description": "Get updates on time"},
-    {
-        "name": "inventory",
-        "description": "Get the current inventory of shop and buying capacity.",
-    },
+    {"name": "hi", "description": "Place potion orders."},
 ]
 
 app = FastAPI(
-    title="Central Coast Cauldrons",
+    title="Course Finder",
     description=description,
     version="0.0.1",
     terms_of_service="http://example.com/terms/",
     contact={
-        "name": "Lucas Pierce",
-        "email": "lupierce@calpoly.edu",
+        "name": "Spencer Perley",
+        "email": "sperley@calpoly.edu",
     },
     openapi_tags=tags_metadata,
 )
 
-origins = ["https://potion-exchange.vercel.app"]
+origins = ["**"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -43,15 +31,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(inventory.router)
-app.include_router(carts.router)
-app.include_router(catalog.router)
-app.include_router(bottler.router)
-app.include_router(barrels.router)
-app.include_router(admin.router)
-app.include_router(info.router)
+app.include_router(hi.router)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Shop is open for business!"}
+    return {"message": "Welcome to Course Finder!"}
