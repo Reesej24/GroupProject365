c6c42f940c057e2cb5e05ee8a33681d7ab0cd874
Jacob Reese <159951545+Reesej24@users.noreply.github.com>
2025-05-12 11:38:24 -0700

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -14,7 +14,7 @@ config = context.config
 config.set_main_option(
     "sqlalchemy.url",
     os.getenv(
-        "POSTGRES_URI", "postgresql+psycopg://myuser:mypassword@localhost/groupdatabase"
+        "POSTGRES_URI", "postgresql+psycopg://myuser:mypassword@localhost/mydatabase"
     ),
 )
 
